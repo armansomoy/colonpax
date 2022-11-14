18fbc20c32c866f0d1cd65e49d2d7709a00afc08
Arman Hossain Somoy <61237257+armansomoy@users.noreply.github.com>
2022-11-14 18:13:22 +0600

    changes

--- src/pages/faq/Faq.jsx
+++ src/pages/faq/Faq.jsx
@@ -1,93 +1,106 @@
-import React from 'react'
-import Accordion from 'react-bootstrap/Accordion';
+import React from "react";
+import Accordion from "react-bootstrap/Accordion";
 import "./Faq.css";
 import free from "../../assets/Group 54.svg";
 import best from "../../assets/Group 52.svg";
 import eco from "../../assets/Group 62.svg";
 const Faq = () => {
   return (
-    <div style={{ background: '#EDF7F0' }} className="py-5 pb-5">
-      <div className='container'>
+    <section id="faq">
+      <div className="container">
         <div className="row">
           <div className="col-md-5">
-            <h2 className='faq_h2'>Häufig gestellte Fragen</h2>
-            <p className='faq-p'>Hier findest Du eine kurze Auswahl an den wichtigsten Fragen. Sollte deine Frage unbeantwortet sein besuche einfach unsere FAQ Seite oder kontaktiere uns.</p>
-            <button className='faq_btn'>KONTAKTIERE UNS</button>
+            <h2 className="faq_h2">Häufig gestellte Fragen</h2>
+            <p className="faq-p">
+              Hier findest Du eine kurze Auswahl an den wichtigsten Fragen.
+              Sollte deine Frage unbeantwortet sein besuche einfach unsere FAQ
+              Seite oder kontaktiere uns.
+            </p>
+            <button className="faq_btn">KONTAKTIERE UNS</button>
           </div>
           <div className="col-md-7">
             <Accordion>
-              <Accordion.Item eventKey="0">
-                <Accordion.Header>Kann Colonpax Blähungen verursachen?</Accordion.Header>
+              <Accordion.Item eventKey="0" className="firstItem">
+                <Accordion.Header>
+                  Kann Colonpax Blähungen verursachen?
+                </Accordion.Header>
                 <Accordion.Body>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                  minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                  aliquip ex ea commodo consequat. Duis aute irure dolor in
-                  reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                  pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                  culpa qui officia deserunt mollit anim id est laborum.
+                  Der Colonpax - Ballaststoffomplex ist gut verträglich und
+                  sicher. Wenn du Colonpax zum ersten Mal verwendest, könntest
+                  du in den ersten Tagen der Anwendung leichte Blähungen
+                  verspüren. Aber bitte keine Sorge, dein Körper muss sich nur
+                  auf die erhöhte Ballaststoffzufuhr einstellen.
                 </Accordion.Body>
               </Accordion.Item>
               <Accordion.Item eventKey="1">
-                <Accordion.Header>Warum sind Ballaststoffe so wichtig?</Accordion.Header>
+                <Accordion.Header>
+                  Warum sind Ballaststoffe so wichtig?
+                </Accordion.Header>
                 <Accordion.Body>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                  minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                  aliquip ex ea commodo consequat. Duis aute irure dolor in
-                  reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                  pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                  culpa qui officia deserunt mollit anim id est laborum.
+                  Ballaststoffe sind wichtig für die Gesundheit , deine
+                  Verdauung und für einen regelmäßigen Stuhlgang. Der Colonpax -
+                  Ballaststoffkomplex sorgt auch für ein längeres
+                  Sättigungsgefühl, und aktiviert den Stoffwechsel.
+                  Ballaststoffe können auch zur Vorbeugung von Krankheiten wie
+                  Diabetes, Herzerkrankungen oder Darmkrebs beitragen.
                 </Accordion.Body>
               </Accordion.Item>
               <Accordion.Item eventKey="2">
-                <Accordion.Header>Wann fängt Colonpax an zu wirken?</Accordion.Header>
+                <Accordion.Header>
+                  Wann fängt Colonpax an zu wirken?
+                </Accordion.Header>
                 <Accordion.Body>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                  minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                  aliquip ex ea commodo consequat. Duis aute irure dolor in
-                  reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                  pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                  culpa qui officia deserunt mollit anim id est laborum.
+                  Das hängt von deiner individuellen Stoffwechselreaktion und
+                  deinem Stoffwechselalter ab und kann bei jedem unterschiedlich
+                  variieren; nach ca. 24–72 Stunden solltest du aber eine
+                  Veränderung bei deinem Stuhlgang bemerken. Wir raten Dir daher
+                  unser kostenfreies Quiz zu machen um zu sehen wie Colonpax bei
+                  Dir wirkt.
                 </Accordion.Body>
               </Accordion.Item>
               <Accordion.Item eventKey="3">
-                <Accordion.Header>Kann ich Colonpax während des Intervall-Fastens einnehmen?</Accordion.Header>
+                <Accordion.Header>
+                  Kann ich Colonpax während des Intervall-Fastens einnehmen?
+                </Accordion.Header>
                 <Accordion.Body>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                  minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                  aliquip ex ea commodo consequat. Duis aute irure dolor in
-                  reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                  pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                  culpa qui officia deserunt mollit anim id est laborum.
+                  Kurz und einfach: Ja, du kannst Colonpax während des
+                  Intervall-Fastens zu dir nehmen und wird dein Fasten auch
+                  nicht brechen.
                 </Accordion.Body>
               </Accordion.Item>
               <Accordion.Item eventKey="4">
-                <Accordion.Header>Kann ich Colonpax während einer Keto-Diät einnehmen?</Accordion.Header>
+                <Accordion.Header>
+                  Kann ich Colonpax während einer <br />
+                   Keto-Diät einnehmen?
+                </Accordion.Header>
                 <Accordion.Body>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                  minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                  aliquip ex ea commodo consequat. Duis aute irure dolor in
-                  reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                  pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                  culpa qui officia deserunt mollit anim id est laborum.
+                  Ja, das kannst du gern machen! Colonpax wird im Gegensatz zu
+                  anderen Mikronährstoffen wie beispielsweise Proteinen oder
+                  Kohlenhydraten, die der Körper abbaut und absorbiert, nicht
+                  verdaut. Stattdessen bleibt der Colonpax - Ballaststoffkomplex
+                  sie relativ intakt in Magen, Dünndarm und Dickdarm. Du wirst
+                  also keine zusätzlichen Kohlenhydrate aufnehmen. Dein Körper
+                  wird die Ballaststoffe weder absorbieren noch in Zucker
+                  zerlegen.
                 </Accordion.Body>
               </Accordion.Item>
             </Accordion>
           </div>
         </div>
-<div className='d-flex justify-content-around service_div'>
-  <div className="service"><img src={free} alt="" className='img-fluid'/> Free Delivery</div>
-  <div className="service"><img src={best} alt="" className='img-fluid'/> Best Choice</div>
-  <div className="service"><img src={eco} alt="" className='img-fluid'/> Eco Product</div>
-</div>
+        <div className="d-flex justify-content-around service_div">
+          <div className="service">
+            <img src={free} alt="" className="img-fluid" /> Kostenfreier Versand
+          </div>
+          <div className="service">
+            <img src={best} alt="" className="img-fluid" /> Sichere Bezahlung
+          </div>
+          <div className="service">
+            <img src={eco} alt="" className="img-fluid" />  Bio Qualität
+          </div>
+        </div>
       </div>
-    </div>
+    </section>
+  );
+};
 
-  )
-}
-
-export default Faq
+export default Faq;
--- src/pages/footer/Footer.jsx
+++ src/pages/footer/Footer.jsx
@@ -1,35 +1,42 @@
-import React from 'react';
+import React from "react";
 import "./Footer.css";
-import logo from "../../assets/Logo2.svg"
-import facebook from "../../assets/Group 114.svg"
-import twitter from "../../assets/Group 115.svg"
-import dot from "../../assets/Group 116.svg"
-import cloud from "../../assets/Union 8.svg"
+import logo from "../../assets/Logo2.svg";
+import facebook from "../../assets/Group 114.svg";
+import twitter from "../../assets/Group 115.svg";
+import dot from "../../assets/Group 116.svg";
+import cloud from "../../assets/Union 8.svg";
 
 const Footer = () => {
   return (
-    <div className='footer_section'>
-
-      <div className='container'>
+    <div className="footer_section">
+      <div className="container">
         <div className="cards row overflow-hidden">
           <div className="col-md-6">
-            <h2 className='card_h2'>Save Your Time And Money</h2>
-            <p className='card_p'>Important fact about learning is that it is a means to improve knowledge.</p>
-            <button className='card_btn'>MACH DEN TEST</button>
+            <h2 className="card_h2">Save Your Time And Money</h2>
+            <p className="card_p">
+              Important fact about learning is that it is a means to improve
+              knowledge.
+            </p>
+            <button className="card_btn">MACH DEN TEST</button>
           </div>
           <div className="col-md-6 card_imges">
-            <img src={cloud} alt="" className='card_img' />
+            <img src={cloud} alt="" className="card_img" />
           </div>
         </div>
-        <div className='row'>
-          <div className='col-md-5'>
-            <img src={logo} alt="logo" className='img-fluid' />
+        <div className="row main-footer">
+          <div className="col-md-5">
+            <img src={logo} alt="logo" className="img-fluid" />
 
-            <p className='footer_p pt-2'>ColonPax ist ein sicherer und effektiver Weg, wie du Verstopfungen auf eine natürliche Weise lindern, Gewicht verlieren und auch Blähungen reduzieren kannst. Beantworte das Quiz und erfahre, wie es dir helfen kann.</p>
+            <p className="footer_p pt-2">
+              ColonPax ist ein sicherer und effektiver Weg, wie du Verstopfungen
+              auf eine natürliche Weise lindern, Gewicht verlieren und auch
+              Blähungen reduzieren kannst. Beantworte das Quiz und erfahre, wie
+              es dir helfen kann.
+            </p>
           </div>
-          <div className='col-md-2'>
+          <div className="col-md-2">
             <ul>
-              MENU
+              <li className="ul-header">MENU</li>
               <li>Symptome</li>
               <li>Lösung</li>
               <li>Ergebnisse</li>
@@ -37,41 +44,38 @@ const Footer = () => {
               <li>Blog</li>
             </ul>
           </div>
-          <div className='col-md-2'>
-            <ul>SERVICE
+          <div className="col-md-2">
+            <ul>
+              <li className="ul-header">SERVICE</li>
               <li>FAQ </li>
               <li>Kundendienst</li>
               <li>Studentenrabatt</li>
               <li>Kontaktiere uns</li>
             </ul>
           </div>
-          <div className='col-md-2 d-flex justify-content-around social_link'>
-            <div>
-              <img src={facebook} alt="" />
-            </div>
-            <div>
-              <img src={twitter} alt="" />
-            </div>
-            <div>
-              <img src={dot} alt="" />
-            </div>
+          <div className="col-md-2 social_link">
+            <ul>
+              <li><a href="#"><i class="fa-brands fa-facebook-f"></i></a></li>
+              <li><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
+              <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
+            </ul>
           </div>
         </div>
         <hr />
         <div className="row">
           <div className="col-md-6">
-            <p className='footer_p'>Copyright © 2022 Colonpax. Alle Rechte vorbehalten.</p>
+            <p className="down-p">
+              Copyright © 2022 Colonpax. Alle Rechte vorbehalten.
+            </p>
           </div>
-          <div className="col-md-3"><p>
-            Nutzungsbedingungen</p></div>
           <div className="col-md-3">
-            Datenschutzbestimmungen
+            <p>Nutzungsbedingungen</p>
           </div>
+          <div className="col-md-3">Datenschutzbestimmungen</div>
         </div>
       </div>
     </div>
+  );
+};
 
-  )
-}
-
-export default Footer
+export default Footer;
